0b95260c3e9eb2562bd090bf6060bdc623a089e8
lofty11 <1053852415@qq.com>
2024-02-22 17:15:03 +0800

    新增插件表单结构

--- front/src/views/appInfoManagement/plugManagement/components/addPlug.vue
+++ front/src/views/appInfoManagement/plugManagement/components/addPlug.vue
@@ -2,26 +2,36 @@
   <!--    新增插件-->
   <el-dialog :visible="addPlugDialogVisible" title="新建插件" @close="close">
     <el-form>
-      <!-- 表单内容 -->
-      <!--      <el-form-item label="插件名称">-->
-      <!--        <el-input v-model="plugInfo.name" />-->
-      <!--      </el-form-item>-->
+      <el-form-item label="插件名称">
+        <!--        <el-input v-model="plugInfo.name" />-->
+        <el-input style="width:80%" />
+      </el-form-item>
 
-      <!--      <el-form-item label="插件用途">-->
-      <!--        <el-input v-model="plugInfo.usage" />-->
-      <!--      </el-form-item>-->
+      <el-form-item label="插件用途">
+        <!--        <el-input v-model="plugInfo.usage" />-->
+        <el-input style="width:80%" />
 
-      <!--      <el-form-item label="插件描述">-->
-      <!--        <el-input v-model="plugInfo.description" type="textarea" maxlength="255" show-word-limit />-->
-      <!--      </el-form-item>-->
+      </el-form-item>
+
+      <el-form-item label="插件描述">
+        <!--        <el-input v-model="plugInfo.description" type="textarea" maxlength="255" show-word-limit />-->
+        <el-input style="width:80%" type="textarea" :rows="4" maxlength="255" show-word-limit />
+      </el-form-item>
       <el-alert
         title="此描述将作为模型识别、规划和调度插件任务的重要参考，请尽量详尽描述此插件各项细节，包括但不限于插件可用功能、应用场景等。"
         type="warning"
       />
       插件参数配置：
-      <!--      <el-form-item>-->
-      <!--        <el-button type="primary" @click="submitPlugInfo">确认</el-button>-->
-      <!--      </el-form-item>-->
+      <el-form-item>
+        <el-row>
+          <el-col span="10">
+            <el-button type="primary" @click="submitPlugInfo">关闭</el-button>
+            <el-button type="primary" @click="submitPlugInfo">确认</el-button>
+          </el-col>
+        </el-row>
+
+      </el-form-item>
+      <el-form-item />
     </el-form>
   </el-dialog>
   <!--    新增插件-->
