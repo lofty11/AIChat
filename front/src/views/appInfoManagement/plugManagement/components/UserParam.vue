16bdddc7adb5c1b463e37a271bb518f260f1a2bb
lofty11 <1053852415@qq.com>
2024-04-20 16:29:13 +0800

    前端用户参数和插件参数对调

--- front/src/views/appInfoManagement/plugManagement/components/UserParam.vue
+++ front/src/views/appInfoManagement/plugManagement/components/UserParam.vue
@@ -1,28 +1,15 @@
 <template>
+
   <div>
     <el-dialog :visible="userParamDialogVisible" :title="dialogTitle" append-to-body @close="close">
       <el-form ref="paramForm" :model="paramForm" :rules="rules" align="right">
         <!-- 表单内容 -->
-        <el-form-item prop="name" label="字段名称">
+        <el-form-item prop="name" label="参数名">
           <el-input v-model="paramForm.name" style="width: 75%" />
         </el-form-item>
 
-        <el-form-item prop="field" label="字段">
-          <el-input v-model="paramForm.field" style="width: 75%" />
-        </el-form-item>
-        <el-form-item prop="type" label="类型">
-          <el-select v-model="paramForm.type" style="width: 75%" placeholder="请选择类型">
-            <el-option v-for="item in typeList" :key="item.id" :label="item.type" :value="item.id" />
-          </el-select>
-        </el-form-item>
-        <el-form-item prop="enumerationRange" label="枚举范围">
-          <el-input v-model="paramForm.enumerationRange" style="width: 75%" />
-        </el-form-item>
-        <el-form-item prop="necessary" label="是否必填" align="left">
-          <el-switch v-model="paramForm.necessary" />
-        </el-form-item>
-        <el-form-item prop="description" label="描述">
-          <el-input v-model="paramForm.description" style="width: 75%" />
+        <el-form-item prop="value" label="参数值">
+          <el-input v-model="paramForm.value" style="width: 75%" />
         </el-form-item>
         <el-form-item>
           <el-row>
@@ -40,7 +27,6 @@
 <script>
 
 import { createUserPara, getUserParaById, modifyUserParaById } from '@/api/plug'
-import { getTypeUnions } from '@/api/common'
 
 export default {
   props: {
@@ -48,90 +34,51 @@ export default {
       type: Boolean,
       default: false
     },
-    dialogTitle: {
-      type: String,
-      default: ''
-    },
-    plugId: {
-      type: String,
-      default: ''
-    },
     userParaId: {
+      type: String,
+      default: '0'
+    },
+    userId: {
+      type: String,
+      default: '0'
+    },
+    dialogTitle: {
       type: String,
       default: ''
     }
   },
   data() {
     return {
-      typeList: [],
       paramForm: {
         name: '',
-        field: '',
-        type: '',
-        enumerationRange: '',
-        necessary: false,
-        description: ''
-      },
-      form: {
-        name: '',
-        field: '',
-        type: '',
-        enumerationRange: '',
-        necessary: 0,
-        description: ''
+        value: ''
       },
       rules: {
-        name: [{ required: true, message: '字段名称不能为空', trigger: 'blur' }],
-        field: [{ required: true, message: '字段不能为空', trigger: 'blur' }],
-        type: [{ required: true, message: '类型不能为空', trigger: 'blur' }]
+        name: [{ required: true, message: '参数名称不能为空', trigger: 'blur' }],
+        value: [{ required: true, message: '参数值不能为空', trigger: 'blur' }]
       }
     }
   },
   watch: {
     userParaId(newValue) {
       if (newValue === '0') {
-        this.paramForm = [
-          {
-            name: '',
-            field: '',
-            type: '',
-            enumerationRange: '',
-            necessary: false,
-            description: ''
-          }
-        ]
+        this.paramForm = { name: '', value: '' }
       } else {
         getUserParaById(newValue).then(response => {
           this.paramForm = response.data
-          console.log(response.data)
-          this.$set(this.paramForm, 'necessary', Boolean(response.data.necessary))
         })
       }
     }
-  },
-  mounted() {
-    getTypeUnions().then(response => {
-      this.typeList = response.data
-    })
   },
   methods: {
     close() {
       this.$emit('update:userParamDialogVisible', false)
     },
-    trans() {
-      this.form.name = this.paramForm.name
-      this.form.field = this.paramForm.field
-      this.form.type = this.paramForm.type
-      this.form.enumerationRange = this.paramForm.enumerationRange
-      this.form.description = this.paramForm.description
-      this.form.necessary = this.paramForm.necessary ? 1 : 0
-    },
     confirm() {
       this.$refs.paramForm.validate(valid => {
         if (valid) {
           if (this.userParaId === '0') {
-            this.trans()
-            createUserPara(this.plugId, this.form).then(response => {
+            createUserPara(this.userId, this.paramForm).then(response => {
               if (response.errno === 1) {
                 this.$message.success('新增用户参数成功！')
                 this.addItem('addUserPara')
@@ -141,17 +88,15 @@ export default {
               this.$emit('update:userParamDialogVisible', false)
             })
           } else {
-            modifyUserParaById(this.userParaId, this.form).then(response => {
-              if (response.errno === 0) {
-                this.$message.success('编辑用户参数成功！')
-              } else {
+            modifyUserParaById(this.userParaId, this.paramForm).then((response) => {
+              if (response.errno === 0) { this.$message.success('编辑用户参数成功！') } else {
                 this.$message.error('编辑用户参数失败')
               }
               this.userParamDialogVisible = false
             })
           }
         } else {
-          this.$message.error('请将表单填写完整！')
+          this.$message.error('请将用户参数填写完整！')
           return false
         }
       })
